6c19706ec06f76eaaa6e08d27b84c3456ae56aef
abimanyu <abimanyutroubleseeker@gmail.com>
2017-10-18 23:33:09 -0700

    Revert "deret bilangan genap"
    This reverts commit 3a79127c1eb3be708bd4196045be048ee2708ceb.

--- genap.py
+++
@@ -1,6 +0,0 @@
-print("DAFTAR BILANGAN GENAP")
-nim = int(input("MASUKKAN NIM ANDA (tanpa 0 di depan) : "))
-loop= int(input("MASUKKAN BERAPA LOOP YANG ANDA INGINKAN : "))
-for i in range(nim,nim+loop):
-	if (i % 2) == 0:
-		print(i)
